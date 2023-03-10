4f3a95980cb2de0ca5dcebdbbc7ce98515a5be22
AebadUQ <aebadulquadir123@gmail.com>
2023-03-10 16:01:05 +0500

    "error fixed"

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,4 @@
 import { Menu, Dropdown, Badge, Drawer, Typography, Image } from 'antd';
-import { Menu, Dropdown, Badge, Drawer, Typography, Image } from 'antd';
 import { HomeOutlined, UserOutlined, SettingOutlined, ShoppingCartOutlined } from '@ant-design/icons';
 import { useDispatch, useSelector } from 'react-redux';
 import { DeleteOutlined } from '@ant-design/icons';
@@ -88,14 +87,6 @@ console.log(price)
     setVisible(false);
   };
 
-  const showDrawer = () => {
-    setVisible(true);
-  };
-
-  const onClose = () => {
-    setVisible(false);
-  };
-
   return (
     <>
       <Menu mode="horizontal" style={{ background: 'linear-gradient(to left, #8538ed, #c4a6f8)', color: 'white' }}>
@@ -110,32 +101,7 @@ console.log(price)
         </Menu.Item>
         <Menu.Item key="cart" icon={<Badge count={getdata?.length} style={{ fontSize: '10px', backgroundColor: '#FADA5E', color: 'black' }}><ShoppingCartOutlined style={{ fontSize: '22px', color: 'white' }} /></Badge>} onClick={showDrawer} style={{ marginLeft: 'auto' }}>
 
-    <>
-      <Menu mode="horizontal" style={{ background: 'linear-gradient(to left, #8538ed, #c4a6f8)', color: 'white' }}>
-        <Menu.Item key="home" >
-          Home
         </Menu.Item>
-        <Menu.Item key="about">
-          About Us
-        </Menu.Item>
-        <Menu.Item key="settings" >
-          Settings
-        </Menu.Item>
-        <Menu.Item key="cart" icon={<Badge count={getdata?.length} style={{ fontSize: '10px', backgroundColor: '#FADA5E', color: 'black' }}><ShoppingCartOutlined style={{ fontSize: '22px', color: 'white' }} /></Badge>} onClick={showDrawer} style={{ marginLeft: 'auto' }}>
-
-        </Menu.Item>
-
-      </Menu>
-      <Drawer
-        title="Studoyo Ecom"
-        placement="right"
-        closable={false}
-        onClose={onClose}
-        visible={visible}
-      >
-        {cartMenu}
-      </Drawer>
-    </>
 
       </Menu>
       <Drawer
